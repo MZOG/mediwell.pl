bedb8537e8a9445b1a7b01cc572978aa1a8134e2
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2025-01-09 15:10:13 +0100

    homepage placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`font-[family-name:var(--font-geist-sans)] antialiased`}>
         {children}
       </body>
     </html>
